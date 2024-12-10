0bbd9038b37e2ba3a1d24f95b22b5b087b9cb91d
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-12-10 12:39:15 +0530

    Initial commit.

---
+++ src/test/java/com/damian/sr/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.sr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
